4cef035f0ccbc6d07b4dba4504c50e083e485d51
Arunasalamganesan <Arunasalamganesan@users.noreply.github.com>
2021-03-15 21:32:45 +0530

    add the

---
+++ src/AddtwoNumber/LocalDateExample1.java
@@ -0,0 +1,16 @@
+package AddtwoNumber;
+
+import java.time.LocalDate;
+
+public class LocalDateExample1 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        LocalDate yesterday = date.minusDays(7);
+        LocalDate tomorrow = yesterday.plusDays(30);
+        System.out.println("Today date: "+date);
+        System.out.println("Yesterday date: "+yesterday);
+        System.out.println("Tommorow date: "+tomorrow);
+        LocalDate r = LocalDate.now();
+        System.out.println(r);
+    }
+}
